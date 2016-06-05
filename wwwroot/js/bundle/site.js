01011c3459b82411657461aeb0c533f97c5d6ca5
Daniel Tebbutt <moeruze.burning@gmail.com>
2016-06-05 14:13:08 +1200

    More

--- wwwroot/js/bundle/site.js
+++ wwwroot/js/bundle/site.js
@@ -67,52 +67,14 @@ function get(url, data, success, error) {
 import { createStore } from 'redux';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Card from 'material-ui/lib/card/card';
-import CardMedia from 'material-ui/lib/card/card-media';
-import CardTitle from 'material-ui/lib/card/card-title';
-import CardText from 'material-ui/lib/card/card-text';
-import FlatButton from 'material-ui/lib/flat-button';
 
-var ProductBox = React.createClass({
-  render: function() {
-    var prod = this.props.product;
-    return (
-      <div className="product-container">
-        <Card style={{ width: "200px", margin: "10px", float: "left" }}>
-          <CardMedia>
-            <img src={ prod.ImageUrl } style={{ height: "200px", objectFit: "cover" }}/>
-          </CardMedia>
-          <CardTitle>{ prod.Name }</CardTitle>
-          <FlatButton label={"Buy - $" + (prod.PriceCents / 100.0) } 
-                      style={{ float: "right" }}
-                      linkButton={true}
-                      href={"https://tabeyo.foxycart.com/cart" +
-                            "?name=" + prod.Name +
-                            "&price=" + (prod.PriceCents / 100) +
-                            "&image=" + prod.ImageUrl }/>
-        </Card>
-      </div>
-    );
-  }
-});
+import ProductBox from "../../components/ProductBox/ProductBox";
+import TabeyoPage from "../../components/TabeyoPage/TabeyoPage";
 
-var ProductList = React.createClass({
-  render: function() {
-    var products = [];
-    this.props.products.forEach(function(product){
-      products.push(<ProductBox product={product} key={product.Name}/>)
-    });
-    return (
-      <div className="products-list">
-        {products}
-      </div>
-    )
-  }
-})
 
-get("/Product", null, function(data) {  
+get("/Product", null, data => {  
   ReactDOM.render(  
-    <ProductList products={data} />,
+    <TabeyoPage />,
     document.getElementById('content')
   );
 });
